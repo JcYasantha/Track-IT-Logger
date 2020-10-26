6ea7652a6bb91ac1eac7e34fe49af2a44aed4de4
JcYasantha <yasantha619@gmail.com>
2020-10-26 15:27:47 +0530

    redux store added and Provider added for App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import { Provider } from 'react-redux';
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
+import store from './store';
 import Navbar from "./layouts/Navbar";
 import TechList from './techs/TechList';
 import AddTech from './techs/AddTech';
@@ -16,17 +18,19 @@ function App() {
   useEffect(() => {
     M.AutoInit();
   });
-  return <div className='App'>
-    <Navbar />
-    <div className="container">
-      <AddBtn />
-      <LogList />
-      <AddLog/>
-      <EditLog />
-      <TechList />
-      <AddTech/>
+  return <Provider store={store}>
+    <div className='App'>
+      <Navbar />
+      <div className="container">
+        <AddBtn />
+        <LogList />
+        <AddLog/>
+        <EditLog />
+        <TechList />
+        <AddTech/>
+      </div>
     </div>
-  </div>;
+  </Provider>;
 }
 
 export default App;
